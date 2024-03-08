b0c2121b256aefe41c8796a1e782e415d980ef6e
zoagan04 <32179885-zoagan04@users.noreply.replit.com>
2024-03-08 18:22:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# gets hour
+hour = int(input('Какой сейчас час? ' ))
+# gets day
+day = int(input("Какой сейчас день? " ))
+# gets month
+month = int(input("Какой сейчас месяц? "))
+# gets year
+year = int(input("Какой сейчас год "))
+# print("Cейчас", hour, +  '.' "часов", day, + '.' "число", month, 'месяц', year, 'год') 
+print("Cейчас", hour,  "часов", day,  "число", month, 'месяц', year, 'год')
